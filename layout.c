b83750a554db9ca1ef4d74367c6d87ee7c2b90b1
Sören Tempel <soeren+git@soeren-tempel.net>
2026-08-09 16:55:32 +0200

    fixup! Treat popups and dialogs as floating windows

--- layout.c
+++ layout.c
@@ -355,7 +355,8 @@ extern void render_seat_focus(struct Seat *seat) {
 	struct Window *window = seat->focused->focused;
 	if (window == NULL || seat->ls_focused)
 		return;
-	river_node_v1_place_top(window->node);
+	if (window->floating)
+		river_node_v1_place_top(window->node);
 	if (seat->focused->layout == monocle_layout)
 		render_border(window, monocle_borderpx, focused_color);
 	else
